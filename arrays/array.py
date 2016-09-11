25ad53d1c78e2c890636505b80b5c831bd110fa0
Dustin Belliston <dustinbelliston@gmail.com>
2016-09-11 00:35:00 -0600

    packaged arrays

--- arrays/array.py
+++ arrays/array.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
-
-import os
-import csv
-import sys
-import argparse
-
-
 class Array(object):
     '''
     An array implementation that holds arbitrary objects.
@@ -158,43 +151,3 @@ def memcpy(dest, source, size):
 def reverse_enum(L):
     for index in reversed(range(len(L))):
         yield index, L[index]
-
-
-def main(arguments):
-    parser = argparse.ArgumentParser(description='Array methods')
-    parser.add_argument(
-        '-f', '--file', type=argparse.FileType('r'), help="File with instruction set")
-    args = parser.parse_args(arguments)
-
-    # instruction_set map
-    instruction_map = {}
-
-    # Step through the instruction set one at a time
-    count = 0
-    with args.file as f:
-        instruction_set = csv.reader(f)
-        for instruction in iter(instruction_set):
-            if count <= 67:
-                ins_func = instruction[0].lower()
-                print('{}: {}'.format(count, instruction))
-                if ins_func == 'create':
-                    test_array = Array()
-                elif ins_func == 'debug':
-                    test_array.debug_print()
-                elif ins_func == 'add':
-                    test_array.add(instruction[1])
-                else:
-                    methodToCall = getattr(test_array, ins_func)
-                    if instruction[2]:
-                        methodToCall(instruction[1], instruction[2])
-                    else:
-                        methodToCall(instruction[1])
-            count += 1
-
-
-    # Close instruction set
-    args.file.close()
-
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
---
+++ arrays/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+import csv
+import sys
+import argparse
+from array import Array
+
+
+def main(arguments):
+    parser = argparse.ArgumentParser(description='Array methods')
+    parser.add_argument(
+        '-f', '--file', type=argparse.FileType('r'), help="File with instruction set")
+    args = parser.parse_args(arguments)
+
+    # instruction_set map
+    instruction_map = {}
+
+    # Step through the instruction set one at a time
+    count = 0
+    with args.file as f:
+        instruction_set = csv.reader(f)
+        for instruction in iter(instruction_set):
+            ins_func = instruction[0].lower()
+            print('{}: {}'.format(count, instruction))
+            if ins_func == 'create':
+                test_array = Array()
+            elif ins_func == 'debug':
+                test_array.debug_print()
+            elif ins_func == 'add':
+                test_array.add(instruction[1])
+            else:
+                methodToCall = getattr(test_array, ins_func)
+                if instruction[2]:
+                    methodToCall(instruction[1], instruction[2])
+                else:
+                    methodToCall(instruction[1])
+            count += 1
+
+    # Close instruction set
+    args.file.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
